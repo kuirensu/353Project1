ab5b41f222d463863055baf84cbd9428abc83fb1
kuiren su <kuirensu@usc.edu>
2018-09-04 15:04:48 -0700

    finished part1

--- client_UDP.py
+++ client_UDP.py
@@ -1,25 +1,58 @@
+from __future__ import print_function
 import socket               # Import socket module
+import argparse             # Import argparse module
+import atexit
 
-if __name__ == "__main__":
+#Part 1 Done
+
+#Required arguments specification
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("-s", "--serverIP", help="indicate the server IP address", required=True)
+parser.add_argument("-p", "--port", type=int, help="port number for client to connect to server", required=True)
+parser.add_argument("-l", "--logfile", help="name of logfile", required=True)
+parser.add_argument("-n", "--name", help="indicates client name", required=True)
+parser.add_argument("--help", action="help", default=argparse.SUPPRESS, help=argparse._("show this help message and exit"))
+args = parser.parse_args()
+# Global output file
+outputFile = open(args.logfile, 'w')
+
+# On exit, log to file
+@atexit.register
+def exitCleanUp():
+    logToFile("terminating client...")
+
+# Logging method
+def logToFile(msg):
+    outputFile.write(msg)
+    outputFile.write("\n")
+    outputFile.flush()
+
+def main():
+    # Socket initialization
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = ('localhost', 12345)
+    serverAddress = (args.serverIP, args.port)
+    print ("[DEBUG] server_address is ", serverAddress)
+    clientName = args.name
+    # Message Type: register <client1>
+    clientRegisterMsg = "register " + clientName
+    sock.sendto(clientRegisterMsg, serverAddress)
+    # log
+    logToFile("connecting to the server " + serverAddress[0] + " at port " + str(serverAddress[1]))
+    logToFile("sending register message " + clientName)
+    print("connected to server and registered\nwaiting for messages...")
+    while True:
+        serverMsg = sock.recvfrom(1024)
+        print("[DEBUG] Received message from server : ", serverMsg[0])
+        serverMsgDataList = serverMsg[0].split()
 
-    try:
-        input = True
-        while(input):
-            user_input=str(raw_input('>Enter string you would like capitalize : '))
-            sock.sendto(user_input, server_address)
-            data, server_detail = sock.recvfrom(1024)
-            print " Received message from server : ", data
-            
-            input = str(raw_input('>Do you want to send more message ? (Y/y/N/n/any character) : '))
-            input = True if input in ['Y', 'y'] else False
-    except:
-        print "Something went wrong while connecting to server"
-    finally:
-        sock.close()
+        if len(serverMsgDataList) == 2 and serverMsgDataList[0] == "welcome" and serverMsgDataList[1].lower() == clientName.lower():
+            serverWelcomeMsg = serverMsgDataList[0]
+            #log print
+            logToFile("received " + serverWelcomeMsg)
+        else:
+            print("[ERROR] incorrect server msg format")
 
-
+main()
 
 
 
--- server_UDP.py
+++ server_UDP.py
@@ -1,15 +1,59 @@
-
+from __future__ import print_function
 import socket               								# Import socket module
+import argparse                                             # Import argparse module
+import atexit
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # Create a socket object
-port = 12345                								# Reserve a port for your service.
-s.bind(('localhost', port))        							# Bind to the port
-print " Binding completed  ! !"
+# Part 1 Done
 
-while True:
-    data, client_address = s.recvfrom(1024)
-    print " Received connection from : ", client_address
-    if (data):
-        print " Data received from client : ", str(data)
-        print " Sending data back to client : ", data.upper()
-        s.sendto(data.upper(), client_address)
+# TODO IP difference between client and server, exit cmd
+# Required arguments specification
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("-p", "--port", type=int, help="the port number for the chat server", required=True)
+parser.add_argument("-l", "--logfile", help="name of logfile", required=True)
+parser.add_argument("-h", "--handler", type=int, help="indicates unknown clients", required=True)
+parser.add_argument("--help", action="help", default=argparse.SUPPRESS, help=argparse._("show this help message and exit"))
+args = parser.parse_args()
+# Global output file
+outputFile = open(args.logfile, 'w')
+
+# On exit, log to file
+@atexit.register
+def exitCleanUp():
+    logToFile("terminating server...")
+
+# Logging method
+def logToFile(msg):
+    #LOCK?
+    outputFile.write(msg)
+    outputFile.write("\n")
+    outputFile.flush()
+
+def main():
+    # Socket initialization
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    port = args.port
+    sock.bind(('localhost', port))
+    print("[DEBUG] Binding completed  ! !")
+    # log
+    logToFile("server started on "+ socket.gethostbyname(socket.getfqdn()) + " at port " + str(port) + "...")
+    while True:
+        clientMsg = sock.recvfrom(1024)
+        print("[DEBUG] Data received from client : ", str(clientMsg[0]))
+        clientAddress = clientMsg[1]
+        # log
+        logToFile("client connection from host " + clientAddress[0] + " port " + str(clientAddress[1]))
+        clientMsgDataList= clientMsg[0].split()
+
+        if len(clientMsgDataList) == 2 and clientMsgDataList[0].lower() == "register":
+            clientName = clientMsgDataList[1]
+            print(clientName, "registered from host", clientAddress[0], "port", str(clientAddress[1]))
+            # Message Type: welcome <client1>
+            welcomeMsg = "welcome " + clientName
+            # log
+            logToFile("received register " + clientName + " from host " + clientAddress[0] + " port " + str(clientAddress[1]))
+            if (clientName):
+                sock.sendto(welcomeMsg, clientAddress)
+        else:
+            print("[ERROR] incorrect client msg format")
+
+main()
