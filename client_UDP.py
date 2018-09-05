80ecd2e4e608035f0c13bf3b305084bc1857dd8f
kuiren su <kuirensu@usc.edu>
2018-09-04 20:13:28 -0700

    added command line exit option

--- client_UDP.py
+++ client_UDP.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 import socket               # Import socket module
 import argparse             # Import argparse module
 import atexit
-
+import thread
+import threading
 #Part 1 Done
 
 #Required arguments specification
@@ -13,6 +14,8 @@ parser.add_argument("-l", "--logfile", help="name of logfile", required=True)
 parser.add_argument("-n", "--name", help="indicates client name", required=True)
 parser.add_argument("--help", action="help", default=argparse.SUPPRESS, help=argparse._("show this help message and exit"))
 args = parser.parse_args()
+# Global variables
+CLIENT_NAME = args.name
 # Global output file
 outputFile = open(args.logfile, 'w')
 
@@ -27,30 +30,46 @@ def logToFile(msg):
     outputFile.write("\n")
     outputFile.flush()
 
-def main():
-    # Socket initialization
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    serverAddress = (args.serverIP, args.port)
-    print ("[DEBUG] server_address is ", serverAddress)
-    clientName = args.name
-    # Message Type: register <client1>
-    clientRegisterMsg = "register " + clientName
-    sock.sendto(clientRegisterMsg, serverAddress)
-    # log
-    logToFile("connecting to the server " + serverAddress[0] + " at port " + str(serverAddress[1]))
-    logToFile("sending register message " + clientName)
-    print("connected to server and registered\nwaiting for messages...")
+# Listener thread log messages recieved from server
+def msgListener(sock):
     while True:
         serverMsg = sock.recvfrom(1024)
         print("[DEBUG] Received message from server : ", serverMsg[0])
         serverMsgDataList = serverMsg[0].split()
-
-        if len(serverMsgDataList) == 2 and serverMsgDataList[0] == "welcome" and serverMsgDataList[1].lower() == clientName.lower():
+        
+        if len(serverMsgDataList) == 2 and serverMsgDataList[0] == "welcome" and serverMsgDataList[1].lower() == CLIENT_NAME.lower():
             serverWelcomeMsg = serverMsgDataList[0]
             #log print
             logToFile("received " + serverWelcomeMsg)
         else:
             print("[ERROR] incorrect server msg format")
+
+def main():
+    # Socket initialization
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverAddress = (args.serverIP, args.port)
+    print ("[DEBUG] server_address is ", serverAddress)
+    
+    # Message Type: register <client1>
+    clientRegisterMsg = "register " + CLIENT_NAME
+    #TODO connect before sendto()
+    sock.sendto(clientRegisterMsg, serverAddress)
+    # log
+    logToFile("connecting to the server " + serverAddress[0] + " at port " + str(serverAddress[1]))
+    logToFile("sending register message " + CLIENT_NAME)
+    print("connected to server and registered\nwaiting for messages...")
+    # Initialize listener thread
+    msgListenerThread = threading.Thread(target=msgListener, args=(sock,))
+    msgListenerThread.setDaemon(True)
+    msgListenerThread.start()
+    while True:
+        cmd = raw_input("> ")
+        print("[DEBUG] Input command :", cmd)
+        if str(cmd) == "exit":
+            break
+        else:
+            print("[DEBUG] Unsupported command")
+
 
 main()
 
--- server_UDP.py
+++ server_UDP.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 import socket               								# Import socket module
 import argparse                                             # Import argparse module
 import atexit
-
+import thread
+import threading
 # Part 1 Done
 
 # TODO IP difference between client and server, exit cmd
@@ -28,6 +29,38 @@ def logToFile(msg):
     outputFile.write("\n")
     outputFile.flush()
 
+def sendWelcomeMsg(clientName, clientAddress, sock):
+    # log
+    logToFile("received register " + clientName + " from host " + clientAddress[0] + " port " + str(clientAddress[1]))
+    if (clientName):
+        # Message Type: welcome <client1>
+        welcomeMsg = "welcome " + clientName
+        sock.sendto(welcomeMsg, clientAddress)
+
+#def sendClientMsgToClient(senderName, receiverName, receiverAddress, sock):
+
+def connectionHandler(sock):
+    while True:
+        clientMsgData, clientAddress = sock.recvfrom(1024)
+        clientMsgDataList = clientMsgData.split()
+        # new client connection
+        if len(clientMsgDataList) == 2 and clientMsgDataList[0].lower() == "register":
+            # log
+            logToFile("client connection from host " + clientAddress[0] + " port " + str(clientAddress[1]))
+            clientName = clientMsgDataList[1]
+            # terminal output
+            print(clientName, "registered from host", clientAddress[0], "port", str(clientAddress[1]))
+            # Initiate thread to send client welcome messages
+            sendWelcomeMsgThread = threading.Thread(target=sendWelcomeMsg, args=(clientName, clientAddress, sock,))
+            sendWelcomeMsgThread.setDaemon(True)
+            sendWelcomeMsgThread.start()
+        # Some client send msg
+        #elif len(clientMsgDataList) > 2 and clientMsgDataList[0] == "sendto":
+            # Log sendto
+            # If reveiver is present/not present
+        #else:
+
+
 def main():
     # Socket initialization
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -36,24 +69,22 @@ def main():
     print("[DEBUG] Binding completed  ! !")
     # log
     logToFile("server started on "+ socket.gethostbyname(socket.getfqdn()) + " at port " + str(port) + "...")
-    while True:
-        clientMsg = sock.recvfrom(1024)
-        print("[DEBUG] Data received from client : ", str(clientMsg[0]))
-        clientAddress = clientMsg[1]
-        # log
-        logToFile("client connection from host " + clientAddress[0] + " port " + str(clientAddress[1]))
-        clientMsgDataList= clientMsg[0].split()
+    # TODO: add thread and command line logic
+    connectionHandlerThread = threading.Thread(target=connectionHandler, args=(sock,))
+    connectionHandlerThread.setDaemon(True)
+    connectionHandlerThread.start()
 
-        if len(clientMsgDataList) == 2 and clientMsgDataList[0].lower() == "register":
-            clientName = clientMsgDataList[1]
-            print(clientName, "registered from host", clientAddress[0], "port", str(clientAddress[1]))
-            # Message Type: welcome <client1>
-            welcomeMsg = "welcome " + clientName
-            # log
-            logToFile("received register " + clientName + " from host " + clientAddress[0] + " port " + str(clientAddress[1]))
-            if (clientName):
-                sock.sendto(welcomeMsg, clientAddress)
+    while True:
+        cmd = raw_input("> ")
+        print("[DEBUG] Input command :", cmd)
+        if str(cmd) == "exit":
+            break
         else:
-            print("[ERROR] incorrect client msg format")
+            print("[DEBUG] Unsupported command")
+
 
 main()
+
+
+
+
